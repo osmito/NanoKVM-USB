c8ec7fa21d600aebe27cf08ce2f55a6c439fd6cb
osmito <jaja@xj8.so-net.ne.jp>
2026-01-14 12:59:16 +0900

    Add Japanese

---
+++ browser/src/i18n/locales/ja.ts
@@ -0,0 +1,85 @@
+const ja = {
+  translation: {
+    serial: {
+      notSupported:
+        'シリアルはサポートされていません。マウスとキーボードを有効にするには、デスクトップ版のChromeブラウザをご使用ください。',
+      failed: 'シリアルの接続に失敗しました。もう一度お試しください'
+    },
+    camera: {
+      tip: '認証を待っています...',
+      denied: '許可が拒否されました',
+      authorize:
+        'リモートデスクトップにはカメラの許可が必要です。ブラウザの設定でカメラを許可してください。',
+      failed: 'カメラの接続に失敗しました。もう一度お試しください。'
+    },
+    modal: {
+      title: 'USBデバイスを選択',
+      selectVideo: 'ビデオ入力デバイスを選択してください',
+      selectSerial: 'シリアルデバイスを選択'
+    },
+    menu: {
+      serial: 'シリアル',
+      keyboard: 'キーボード',
+      mouse: 'マウス'
+    },
+    video: {
+      resolution: '解像度',
+      scale: '倍率',
+      rotation: '回転',
+      customResolution: 'カスタム',
+      device: 'デバイス',
+      custom: {
+        title: 'カスタム解像度',
+        width: '幅',
+        height: '高さ',
+        confirm: 'OK',
+        cancel: 'キャンセル'
+      }
+    },
+    keyboard: {
+      paste: '貼り付け',
+      virtualKeyboard: 'キーボード',
+      shortcut: {
+        title: 'ショートカット',
+        custom: 'カスタム',
+        capture: 'ここをクリックしてショートカットをキャプチャ',
+        clear: 'クリア',
+        save: '保存',
+        captureTips:
+          'システムレベルのキー(Windowsキーなど)をキャプチャするには、フルスクリーン権限が必要です。',
+        enterFullScreen: 'フルスクリーンモードに切り替え。'
+      }
+    },
+    mouse: {
+      cursor: {
+        title: 'カーソル',
+        pointer: 'ポインター',
+        grab: 'グラブ',
+        cell: 'セル',
+        hide: '非表示'
+      },
+      mode: 'マウスモード',
+      absolute: '絶対モード',
+      relative: '相対モード',
+      direction: 'ホイールの方向',
+      scrollUp: '上にスクロール',
+      scrollDown: '下にスクロール',
+      speed: 'ホイール速度',
+      fast: '速い',
+      slow: '遅い',
+      requestPointer: '相対モードを使用中です。デスクトップをクリックしてマウスポインターを取得してください。',
+      jiggler: {
+        title: 'マウスジグラー',
+        enable: '有効',
+        disable: '無効'
+      }
+    },
+    settings: {
+      language: '言語',
+      document: 'ドキュメント',
+      download: 'ダウンロード'
+    }
+  }
+};
+
+export default ja;
